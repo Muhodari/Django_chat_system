c45d041baf96361d18039949c8f3b33ec5f2eba4
Muhodari Sage <sagemuho@gmail.com>
2021-07-26 02:57:41 +0200

    creating different views

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Room,Messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def room(request):
+    return
+
+
+def checkview(request):
+    pass
