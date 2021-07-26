ab47f834d28b2d0518ed8ea1b14ee899c03bfed0
Muhodari Sage <sagemuho@gmail.com>
2021-07-26 03:08:48 +0200

    register new room and easy redirect

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Room,Messages
+from django.shortcuts import render, redirect
+from .models import Room, Messages
+
 
 # Create your views here.
 def home(request):
@@ -7,8 +8,16 @@ def home(request):
 
 
 def room(request):
-    return
+    return render(request, 'chatRoom.html')
 
 
 def checkview(request):
-    pass
+    room_name = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room_name).exists():
+        return redirect('/' + room_name + '/?username=' + username)
+    else:
+        new_room = Room.objects.create(name=room_name)
+        new_room.save()
+        return redirect('/' + room_name + '/?username=' + username)
